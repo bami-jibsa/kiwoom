6427075fe944b5f77a69353aa0958ac48e7647dc
bami-jibsa <tlswjddn0615@naver.com>
2024-06-25 05:36:52 +0900

    __pycache__/login.cpython-310.pyc

--- main.py
+++
@@ -1,5 +0,0 @@
-from check_connect
-from get_code
-from get_name
-from check_connect
-from login
--- stock_status.py
+++
@@ -1,7 +0,0 @@
-from pykiwoom.kiwoom import *
-
-kiwoom = Kiwoom()
-kiwoom.CommConnect(block=True)
-
-감리구분 = kiwoom.GetMasterConstruction("005930")
-print(감리구분)
