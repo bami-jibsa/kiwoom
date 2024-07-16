7ecc710f9eef8d368d99cf89433f5aeaf3748ba2
bami_jibsa <tlswjddn0615@naver.com>
2024-07-17 00:30:40 +0900

    __pycache__

--- code/main.py
+++ code/main.py
@@ -19,22 +19,9 @@ n = int(input())
 if __name__ == "__main__":
     if n:
         login()
+    else:
+        pass
 
-from last_price import last_price
-
-from pykiwoom import kiwoom
-
-from condition_list import condition_list
-
-from login_info import log_info
-
-from jango import Kiwoom1
-
-from stock_buy import place_limit_buy_order, place_market_buy_order
-
-from stock_sell import place_limit_sell_order, place_market_sell_order
-
-from get_name import get_name
 
 
 input = sys.stdin.readline
@@ -101,6 +88,7 @@ def start():
                 log_info = log_info
         if n == 3:
             # 조건식 검색
+            from condition_list import condition_list
             print("몇번째 조건식?")
             print("('000', '새조건명'), ('001', '밥번자리'), ('002', '3봉이내 후행스팬 전환선 돌파'), ('003', 'macd'), ('004', 'rsi + macd')")
             c_num = int(input())
@@ -108,6 +96,8 @@ def start():
             print(cond_li)
         if n == 4:
             #종가
+
+            from last_price import last_price
             print("주식번호")
             lp_num = int(input())
             lst_prc = last_price(lp_num)
@@ -118,27 +108,32 @@ def start():
         if n == 0:
             start()
         if n == 1:
-            ("지정가매수")
+            from stock_buy import place_limit_buy_order
+            print("지정가매수")
             print("name 파라미터 int로")
             name, code, num, price = map(int, input().split())
             place_limit_buy_order(name, code, num, price)
         if n == 2:
+            from stock_buy import place_market_buy_order
             print("시장가매수")
             print("name 파라미터 int로")
             name, code, num = map(int, input().split())
             place_market_buy_order(name, code, num)
         if n == 3:
+            from stock_sell import place_limit_sell_order
             ("지정가매도")
             print("name 파라미터 int로")
             name, code, num, price = map(int, input().split())
             place_limit_sell_order(name, code, num, price)        
         if n == 2:
+            from stock_sell import place_market_sell_order
             print("시장가매도")
             print("name 파라미터 int로")
             name, code, num = map(int, input().split())
             place_market_sell_order(name, code, num)
     if k == 4:
         # 잔고 확인
+        from jango import Kiwoom1
         print(f"총매입금액: {Kiwoom1.total_buy_money}원")
         print(f"총평가금액: {Kiwoom1.total_evaluation_money}원")
         print(f"총평가손익금액: {Kiwoom1.total_evaluation_profit_and_loss_money}원")
@@ -146,6 +141,11 @@ def start():
 
 def auto_start(num):
     for i in range(1, 4):
+        from condition_list import condition_list
+        from jango import Kiwoom1
+        from stock_buy import place_market_buy_order
+        from last_price import last_price
+
         cond_li = condition_list(num)
         buy_code = cond_li[i-1]
 
@@ -157,7 +157,7 @@ def auto_start(num):
 print("자동(1)/수동(0)")    
 if __name__ == "__main__":
     type_ = input()
-    if type_:
+    if not type_:
         print("조건식 번호")
         num = int(input())
         auto_start(num)
